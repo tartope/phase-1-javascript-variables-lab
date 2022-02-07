7de8160a64055b2bf8210ee1e3a1a3b7e95bab91
Tunisia Artope <artope@yahoo.com>
2022-02-07 09:05:26 -0500

    commit tree 5b0d91675557b16eacb2dc6ea728791b5811036a

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const companyName = 'Scuber';
+
+// let mostProfitableNeighborhood = 'Chelsea';
+
+// let companyCeo = 'Susan Smith';
